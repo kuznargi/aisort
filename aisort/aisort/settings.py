c4dc58079f3728a7228b2e1d9bd244e4fa6913e4
Nargiza <kuzybahovanargiza@gmail.com>
2025-01-19 20:49:58 +0500

    settings

--- aisort/aisort/settings.py
+++ aisort/aisort/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87jrx4e2(t#yb(a#e32iv#k22rzkz9l249w9odmvkjxges$g#+'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(' ')
 
 
 # Application definition
